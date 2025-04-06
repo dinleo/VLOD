c99f73cf6bfc1aaa4824d7bff4aed58ee4171bf1
Desktop <dinleo11@gmail.com>
2025-04-07 02:38:01 +0900

    Kaggle

--- GroundingDINO/coco.sh
+++ GroundingDINO/coco.sh
@@ -2,5 +2,5 @@ CUDA_VISIBLE_DEVICES=0 \
 python demo/test_ap_on_coco.py \
  -c groundingdino/config/GroundingDINO_SwinB_cfg.py \
  -p output/weights/org_b.pth \
- --anno_path /kaggle/input/coco-val/labels.json \
- --image_dir /kaggle/input/coco-val/image
+ --anno_path /kaggle/input/coco-val/data/labels.json \
+ --image_dir /kaggle/input/coco-val/data/image
