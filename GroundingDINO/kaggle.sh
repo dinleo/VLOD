285302a3b8669b46a7534050bec038f7cbb7569f
Desktop <dinleo11@gmail.com>
2025-04-07 01:48:02 +0900

    Kaggle

--- GroundingDINO/kaggle.sh
+++ GroundingDINO/kaggle.sh
@@ -1,4 +1,4 @@
 export CUDA_HOME=/usr/local/cuda
-cd GroundingDINO && pip install -e .
-mkdir GroundingDINO/groundingdino/weights
-cd GroundingDINO/groundingdino/weights && wget -q https://github.com/IDEA-Research/GroundingDINO/releases/download/v0.1.0-alpha2/groundingdino_swinb_cogcoor.pth
+pip install -e .
+mkdir groundingdino/weights
+cd groundingdino/weights && wget -q https://github.com/IDEA-Research/GroundingDINO/releases/download/v0.1.0-alpha2/groundingdino_swinb_cogcoor.pth
