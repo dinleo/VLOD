22cb09dae5855860690ef6cbad76fa4a3a061a51
Desktop <dinleo11@gmail.com>
2025-04-07 02:17:33 +0900

    Kaggle

--- GroundingDINO/kaggle.sh
+++ GroundingDINO/kaggle.sh
@@ -1,5 +1,6 @@
 export CUDA_HOME=/usr/local/cuda
 pip install wandb huggingface_hub python-dotenv
 pip install -e .
-mkdir -p groundingdino/output/weights
+mkdir -p groundingdino/output
 python utils/hf_down.py
+mv $SUB/weights output/weights
