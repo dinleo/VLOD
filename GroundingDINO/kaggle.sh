a25e6404b97f8b02effc0b2a136e8c41ab5c98f8
Desktop <dinleo11@gmail.com>
2025-04-08 04:14:26 +0900

    train

--- GroundingDINO/kaggle.sh
+++ GroundingDINO/kaggle.sh
@@ -3,5 +3,5 @@ pip install wandb huggingface_hub python-dotenv
 pip install -e .
 mkdir -p output/weights
 python utils_/hf_down.py
-mv org/weights output/weights
+mv origin/weights output/weights
 python utils_/add_coco_obj.py
