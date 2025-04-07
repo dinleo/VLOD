b015d6fb8fdf811ffc54fdf53689f672a16e51c8
Desktop <dinleo11@gmail.com>
2025-04-08 04:11:42 +0900

    train

--- GroundingDINO/train.sh
+++ GroundingDINO/train.sh
@@ -3,4 +3,4 @@ python demo/train.py \
  -c groundingdino/config/GroundingDINO_SwinB_cfg.py \
  -p output/weights/org_b.pth \
  --anno_path $COCODATA/annotations/labels.json \
- --image_dir $COCODATA/image
+ --image_dir $COCODATA/train2017
