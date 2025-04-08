308105e089be11ed086b678811a80eb631647d7c
Mac <dinleo11@gmail.com>
2025-04-08 16:47:49 +0900

    train obj

---
+++ GroundingDINO/train_obj.sh
@@ -0,0 +1,6 @@
+CUDA_VISIBLE_DEVICES=0 \
+python demo/train.py \
+ -c groundingdino/config/GroundingDINO_SwinB_cfg.py \
+ -p output/weights/org_b.pth \
+ --anno_path $WORKING/annotations/labels_obj.json \
+ --image_dir $COCODATA/train2017
