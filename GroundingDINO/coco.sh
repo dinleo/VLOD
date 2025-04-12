6d5da1981300f8d53ac075ee9a4d647b665c19fe
Desktop <dinleo11@gmail.com>
2025-04-13 00:12:56 +0900

    test1

--- GroundingDINO/coco.sh
+++ GroundingDINO/coco.sh
@@ -2,6 +2,6 @@ CUDA_VISIBLE_DEVICES=0 \
 python demo/test_ap_on_coco.py \
  -c groundingdino/config/GroundingDINO_SwinB_cfg.py \
  -p output/weights/org_b.pth \
- --anno_path $WORKING/annotations/labels.json \
- --image_dir $COCODATA/train2017 \
+ --anno_path $WORKING/data/annotations/labels.json \
+ --image_dir $COCODATA/data/train2017 \
  --num_sample 2000
