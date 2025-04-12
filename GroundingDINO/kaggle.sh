73f5b38fdaf46865cf15e1b45c0ec75e6b8afbe5
Desktop <dinleo11@gmail.com>
2025-04-13 00:21:35 +0900

    test2

--- GroundingDINO/kaggle.sh
+++ GroundingDINO/kaggle.sh
@@ -1,7 +1,7 @@
 export CUDA_HOME=/usr/local/cuda
 pip install wandb huggingface_hub python-dotenv
 pip install -e .
-mkdir -p output
+mkdir -p output/data
 mkdir -p annotations
 python utils_/hf_down.py --sub $SUB --filename $CKPT
 mv $SUB/weights output/weights
